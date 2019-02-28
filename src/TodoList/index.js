237b68a51c5a24931cf80fa800c240e05240e63b
Mike Green <mike.green@simplethread.com>
2019-02-28 13:07:41 -0500

    Remove unused import in todo list

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../AppContext';
 import TaskList from './TaskList';
 import TaskForm from './TaskForm';
