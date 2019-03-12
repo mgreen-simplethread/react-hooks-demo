47ebf68d8864a07a0b7e550af1ccdbd573bb0b42
Mike Green <mike.green@simplethread.com>
2019-03-12 17:55:59 -0400

    Remove unused context hook import

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useAppContext } from '../AppContext';
 import TaskList from './TaskList';
 import TaskForm from './TaskForm';
